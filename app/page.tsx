9a2489ae696dce5822326bbe20f4ef730accce30
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2025-03-28 11:04:39 -0300

    feat(1h32): implementa BgGradient

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 "use client";
 
+import { BgGradient } from "@/components/common/bg-gradient";
 import { HeroSection } from "@/components/home/hero-section";
 
 export default function Home() {
   return (
     <div className='relative w-full'>
-      <HeroSection />
-      {/* <DemoSection/> */}
+      <BgGradient />
+      <div className='flex flex-col'>
+        <HeroSection />
+        {/* <DemoSection/> */}
+      </div>
       {/* <HowItWorksSection /> */}
       {/* <PricingSection /> */}
       {/* <CTASection /> */}
