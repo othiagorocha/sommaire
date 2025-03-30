90dc9762b2dd1bb6a1d0e6899d214597ff3eb1aa
Thiago Rocha <thiago_srocha01@hotmail.com>
2025-03-30 14:34:32 -0300

    feat(1h49): implementa HowItWorksSection

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,17 @@
 import { BgGradient } from "@/components/common/bg-gradient";
 import { HeroSection } from "@/components/home/hero-section";
 import { DemoSection } from "@/components/home/demo-section";
+import { HowItWorksSection } from "@/components/home/how-it-works-section";
 
 export default function Home() {
   return (
-    <div className='relative w-full'>
+    <div className="relative w-full">
       <BgGradient />
-      <div className='flex flex-col'>
+      <div className="flex flex-col">
         <HeroSection />
         <DemoSection />
+        <HowItWorksSection />
       </div>
-      {/* <HowItWorksSection /> */}
       {/* <PricingSection /> */}
       {/* <CTASection /> */}
     </div>
