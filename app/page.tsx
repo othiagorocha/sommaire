b1f8de89d509d5f55ba45a67e0ab9bffd8180a16
Thiago Rocha <ti.desenvolvimento@lavive.com.br>
2025-04-02 16:47:23 -0300

    feat(2h05): implementa PricingSection

--- app/page.tsx
+++ app/page.tsx
@@ -4,17 +4,18 @@ import { BgGradient } from "@/components/common/bg-gradient";
 import { HeroSection } from "@/components/home/hero-section";
 import { DemoSection } from "@/components/home/demo-section";
 import { HowItWorksSection } from "@/components/home/how-it-works-section";
+import { PricingSection } from "@/components/home/pricing-section";
 
 export default function Home() {
   return (
-    <div className="relative w-full">
+    <div className='relative w-full'>
       <BgGradient />
-      <div className="flex flex-col">
+      <div className='flex flex-col'>
         <HeroSection />
         <DemoSection />
         <HowItWorksSection />
+        <PricingSection />
       </div>
-      {/* <PricingSection /> */}
       {/* <CTASection /> */}
     </div>
   );
